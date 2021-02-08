2e5e3b0fc414a27dd408174b813ce07a93dc5398
edksam <paakofisam.sam@gmail.com>
2021-02-08 10:49:24 +0000

    Final core functionality added

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API call
-const count = 10;
+const count = 30;
 const apiKey = "fK_a4FkTSAsotRPECh-qzR3FmRzAlxYWweCgiPrDmL0";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Check if all images were loaded.
 function imageLoaded() {
-  console.log("loaded");
+  imagesLoaded++;
+  console.log(imagesLoaded);
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    // console.log("ready =", ready);
+  }
 }
 
 // Helper Function to Set Attributes on DOM Elements
@@ -21,6 +30,9 @@ function setAttributes(element, attributes) {
 }
 // Create Elements for Links & Photos, Add to DOM
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  console.log("total images", totalImages);
   // Run function for each object in photosArray
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash</a>
@@ -43,7 +55,7 @@ function displayPhotos() {
       title: photo.alt_description,
     });
     // Event Listener, check when each is finished loading
-    img.addEventListener("load", imageLoaded());
+    img.addEventListener("load", imageLoaded);
 
     // Put <img> inside <a>, and then put both inside imageContainer
     item.appendChild(img);
@@ -65,9 +77,10 @@ async function getPhotos() {
 // heck to see if scrolling near bottom of page, Load More Photos
 window.addEventListener("scroll", () => {
   if (
-    window.innerHeight + window.scrollY >=
-    document.body.offsetHeight - 1000
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
   ) {
+    ready = false;
     getPhotos();
   }
 });
